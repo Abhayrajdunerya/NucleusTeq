30bd85e5a3c0e12da47bc23c2e5696cce6551e95
Abhayrajdunerya <Abhayrajdunerya@gmail.com>
2024-05-30 18:53:45 +0530

    Fix bug in Global Exception Handler inside server

--- projects/capstone-projects/_2_Interactive_Quiz_Game_With_Backend/server/src/main/java/com/quizify/quizify/controller/QuestionController.java
+++ projects/capstone-projects/_2_Interactive_Quiz_Game_With_Backend/server/src/main/java/com/quizify/quizify/controller/QuestionController.java
@@ -12,12 +12,14 @@ import lombok.Setter;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping(path = "/api", produces = {MediaType.APPLICATION_JSON_VALUE})
+@Validated
 public class QuestionController {
 
     private final IQuestionService iQuestionService;
@@ -40,7 +42,7 @@ public class QuestionController {
     public ResponseEntity<ResponseDto> saveQuestions(@Valid @RequestBody List<QuestionDto> questionDtoList) {
 
         String message = iQuestionService.saveQuestions(questionDtoList);
-        return ResponseEntity.status(HttpStatus.OK).body(new ResponseDto(QuizConstants.STATUS_201, QuizConstants.MESSAGE_201));
+        return ResponseEntity.status(HttpStatus.CREATED).body(new ResponseDto(QuizConstants.STATUS_201, QuizConstants.MESSAGE_201));
 
     }
 
